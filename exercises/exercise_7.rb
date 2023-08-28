96d802a21a71406a075876aeadabdbe0dbf2ac04
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-28 15:07:32 -0400

    Add more inputs

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -13,11 +13,32 @@ puts "----------"
 print "Enter store name: "
 store_name = gets.chomp
 
-# Attempt to create a store with the inputted name
-store = Store.new(name: store_name)
+print "Enter store revenue: "
+store_annual_revenue = gets.chomp
+
+print "Enter employee first name: "
+employee_firstname = gets.chomp
+
+print "Enter employee last name: "
+employee_lastname = gets.chomp
+
+print "Enter employee hourly rate: "
+employee_hourly_rate = gets.chomp
+
+print "Enter employee store id: "
+employee_store_id = gets.chomp
+
+store = Store.new(name: store_name, annual_revenue: store_annual_revenue)
+employee = Employee.new(store_id: employee_store_id, first_name: employee_firstname, last_name: employee_lastname, hourly_rate: employee_hourly_rate)
 
 if store.save
-  puts "Saved"
+  puts "Store Saved"
 else
   puts store.errors.full_messages
+end
+
+if employee.save
+  puts "Employee Saved"
+else
+  puts employee.errors.full_messages
 end
