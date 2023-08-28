f329e9be611f24b9394ecfae4db22c5c3c7ba35e
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-27 21:34:09 -0400

    ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@all_stores = Store.sum(:annual_revenue)
+
+puts "Total annual revenue of all stores: #{@all_stores}"
+
+@total_mil_stores = Store.where("annual_revenue >= 1000000").count
+
+puts @total_mil_stores
